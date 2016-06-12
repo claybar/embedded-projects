40923e3d2a147877b3db44a5b8bdaced15b03c4d
Philip Barclay <philipbarclay@gmail.com>
2016-06-12 22:52:00 +1200

    Helper library removed for now

--- libraries/HomeAutomationHelpers/HomeAutomationHelpers.cpp
+++
@@ -1,9 +0,0 @@
-/*
-  MqttComms.cpp - Library for mqtt comms of my home-automation projects.
-  Created by Philip barclay, May 25, 2016.
-  Released into the public domain.
-*/
-
-//#include "Arduino.h"
-//#include "HomeAutomationHelpers.h"
-
