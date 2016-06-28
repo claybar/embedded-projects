dd654942551a13ebe59ef8607ffe8c896d6a48a7
Philip Barclay <philip.barclay@aranzmedical.com>
2016-06-28 14:25:18 +1200

    Added NTP settings.  Need to get the timezone right.

--- libraries/settings/Secrets.h
+++ libraries/settings/Secrets.h
@@ -7,3 +7,11 @@
 #define MQTT_USERNAME "USERNAME"
 #define MQTT_PASSWORD "PASSWORD"
 
+// Time keeping
+#define NTP_SERVER_IP { 000, 000, 000, 000 }
+const int timeZone = 1;     // Central European Time
+//const int timeZone = -5;  // Eastern Standard Time (USA)
+//const int timeZone = -4;  // Eastern Daylight Time (USA)
+//const int timeZone = -8;  // Pacific Standard Time (USA)
+//const int timeZone = -7;  // Pacific Daylight Time (USA)
+
