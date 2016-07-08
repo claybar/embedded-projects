1c75ca9fb6508c516c498697d8474516e380938f
Philip Barclay <philipbarclay@gmail.com>
2016-07-08 16:47:16 +1200

    Expanded settings to include basic common MQTT stuff and also a settings chunk for the shed controller.

--- libraries/settings/Settings.h
+++ libraries/settings/Settings.h
@@ -1,6 +1,16 @@
 unsigned int version;
 
-struct settings0_t
+struct commonSettings0_t
 {
-  char deviceName[16];
-} settings;
+  const uint8_t version = 0;
+  char deviceName[12];  // Also used as mqttClientID
+  char mqttTopicBase[12];
+  char mqttWillTopic[16];
+  char mqttWillMessage[16];
+};
+
+struct bikeshedControllerSettings0_t
+{
+  const uint8_t version = 0;
+  unsigned long outsideLightAfterMotionTime;  // milliseconds
+};
