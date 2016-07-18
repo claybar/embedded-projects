22c7aa9575d56c25933a161a3c18705a8f017109
Philip Barclay <philipbarclay@gmail.com>
2016-07-18 16:56:17 +1200

    Attempt at using MQTT.  Sort of works, but not constantly 2sec between messages, and randomly locks up.  Seems like some form of background processing blocking or not being processed nicely.

--- mqttEspRelay/src/mqttEspRelay.cpp
+++ mqttEspRelay/src/mqttEspRelay.cpp
@@ -1,10 +1,23 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 #include <SerialCommand.h>
 #include <mqttEspRelay.h>
+#include <PubSubClient.h>
 
-String DeviceName = "ESP8266_" + String(ESP.getChipId());
+#include <Secrets.h>
 
-SerialCommand serialCmd;
+const char* DeviceName = "ESP8266_" + ESP.getChipId();
+
+WiFiClient espClient;
+
+IPAddress mqttIP(MQTT_SERVER_IP);
+PubSubClient mqttClient(espClient);
+//SerialCommand serialCmd;
+
+long lastMsg = 0;
+long now;
+char msg[50];
+int value = 0;
 
 void setup(void)
 {
@@ -15,18 +28,77 @@ void setup(void)
   Serial.println(DeviceName);
 
   Serial.println(F("SER: setup"));
-  serialCmd.addCommand("MQTT", serialMQTTRelay);  // Relay for MQTT message using 2 parameters
-  serialCmd.setDefaultHandler(serialUnrecognized);
+  //serialCmd.addCommand("MQTT", serialMQTTRelay);  // Relay for MQTT message using 2 parameters
+  //serialCmd.setDefaultHandler(serialUnrecognized);
+
+  Serial.println(F("WIFI: setup"));
+  setup_wifi();
+
+  mqttClient.setServer(mqttIP, 1883);
+  mqttClient.setCallback(mqttCallback);
 }
 
 void loop(void)
 {
-  serialCmd.readSerial();
+  //serialCmd.readSerial();
+
+  if (!mqttClient.connected())
+  {
+    mqttReconnect();
+  }
+  mqttClient.loop();
+
+  wdt_reset();
+
+  now = millis();
+  if (now - lastMsg > 2000)
+  {
+    Serial.print("RSSI: ");
+    Serial.println(WiFi.RSSI());
+
+    lastMsg = now;
+
+    Serial.print("Now: ");
+    Serial.println(now);
+
+    Serial.print("Heap: ");
+    Serial.println(ESP.getFreeHeap());
+
+    ++value;
+    snprintf (msg, 75, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    mqttClient.publish("outTopic", msg);
+  }
+}
+
+void setup_wifi()
+{
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(AP_SSID);
+
+  WiFi.begin(AP_SSID, AP_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 /*-------- Serial Monitor ----------*/
+
 void serialMQTTRelay()
 {
+}
+/*
   //int aNumber;
   char *topic;
   char *payload;
@@ -64,9 +136,43 @@ void serialMQTTRelay()
     //mqttPublishRelay(topic, payload);
   }
 }
+*/
 
 // This gets set as the default handler, and gets called when no other command matches.
 void serialUnrecognized(const char *command)
 {
-  Serial.println(F("SER: ??"));
+  //Serial.println(F("SER: ??"));
+}
+
+void mqttReconnect()
+{
+  // Loop until we're reconnected
+  while (!mqttClient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+
+    if (mqttClient.connect(DeviceName))
+    {
+      Serial.println("connected");
+      mqttClient.publish("outTopic", "connected");
+      mqttClient.subscribe("inTopic");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
 }
--- mqttEspRelay/src/mqttEspRelay.h
+++ mqttEspRelay/src/mqttEspRelay.h
@@ -1,2 +1,7 @@
+void setup_wifi();
+
 void serialMQTTRelay();
 void serialUnrecognized(const char *command);
+
+void mqttCallback(char* topic, byte* payload, unsigned int length);
+void mqttReconnect();
