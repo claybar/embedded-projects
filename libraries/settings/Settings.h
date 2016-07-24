34824bd30de1950e5e0bb03ade1fb1c92528a001
Philip Barclay <philipbarclay@gmail.com>
2016-07-24 22:23:58 +1200

    Added settings for front steps controller.

--- libraries/settings/Settings.h
+++ libraries/settings/Settings.h
@@ -14,4 +14,16 @@ struct bikeshedControllerSettings0_t
   const uint8_t version = 0;
   unsigned long outsideLightAfterMotionTime;  // milliseconds
   uint16_t sunlightThreshold;  // level of sunlight to turn lights on
+  uint8_t insideLightsBrightness;
+  uint8_t outsideLightsBrightness;
+};
+
+struct frontstepsControllerSettings0_t
+{
+  const uint8_t version = 0;
+  unsigned long lightingAfterMotionTime;  // milliseconds
+  uint8_t lightingLevelOff; // percentage
+  uint8_t lightingLevelAmbient; // percentage
+  uint8_t lightingLevelBright;  // percentage
+  int lightingChangeTime;  // milliseconds
 };
