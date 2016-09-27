d6874c139c95e70615c9ff46e13d773a255b2847
Philip Barclay <philipbarclay@gmail.com>
2016-09-27 21:40:34 +1300

    General catchup with library naming changes.  Added temp/humidity readings.

--- hwc_monitor/src/hwcMonitor.cpp
+++ hwc_monitor/src/hwcMonitor.cpp
@@ -1,44 +1,66 @@
 #include <Wire.h>
 #include <Homie.h>
 #include <Adafruit_ADS1015.h>
+#include <Adafruit_HTU21DF.h>
 
 #define FW_NAME "hwmonitor"
 #define FW_VERSION "0.0.1"
 
 #define ADCCH_TOP 0
-#define ADCCH_MID 1
-#define ADCCH_BOT 2
-#define ADCCH_REF 3
+#define ADCCH_MIDDLE 1
+#define ADCCH_BOTTOM 2
+#define ADCCH_COLLECTOR 3
 
 Adafruit_ADS1015 ads;
+Adafruit_HTU21DF htu = Adafruit_HTU21DF();
 
-HomieNode tempTopNode("temperatureTop", "temperature");  // ID, type
-HomieNode tempMidNode("temperatureMid", "temperature");  // ID, type
-HomieNode tempBotNode("temperatureBot", "temperature");  // ID, type
+HomieNode cylinderNode("hotwaterCylinder", "hwcylinder");
+HomieNode collectorNode("solarCollector", "solarcollector");
+HomieNode cupboardNode("cupboard", "temperature,humidity");  // ID, type
+
+//HomieNode cylinderNode("cylinder", "temperature");  // ID, type
+//HomieNode collectorNode("collector", "temperature");  // ID, type
+
+const int TRANSMIT_INTERVAL = 10;
+unsigned long lastDataSent = 0;
+//float temperature = 1.0;
 
 void homieLoopHandler()
 {
-  // Read common reference to get mid of VCC rail
-  uint16_t refAdc = ads.readADC_SingleEnded(ADCCH_REF);
+  if (millis() - lastDataSent >= TRANSMIT_INTERVAL * 1000UL)
+  {
+    uint16_t collAdc = ads.readADC_SingleEnded(ADCCH_COLLECTOR);
 
-  uint16_t topAdc = ads.readADC_SingleEnded(ADCCH_TOP);
-  uint16_t midAdc = ads.readADC_SingleEnded(ADCCH_MID);
-  uint16_t botAdc = ads.readADC_SingleEnded(ADCCH_BOT);
+    uint16_t topAdc = ads.readADC_SingleEnded(ADCCH_TOP);
+    uint16_t midAdc = ads.readADC_SingleEnded(ADCCH_MIDDLE);
+    uint16_t botAdc = ads.readADC_SingleEnded(ADCCH_BOTTOM);
 
-  /*
-  if (sensorValue != lastSensorValue) {
-    if (Homie.setNodeProperty(doorNode, "state", sensorValue ? "OPEN" : "CLOSED", true)) {
-      lastSensorValue = sensorValue;
-    }
+    Homie.setNodeProperty(collectorNode, "raw", String(collAdc), true);
+
+    Homie.setNodeProperty(collectorNode, "temperature", String(collAdc), true);
+
+
+    Homie.setNodeProperty(cylinderNode, "rawTop", String(topAdc), true);
+    Homie.setNodeProperty(cylinderNode, "rawMiddle", String(midAdc), true);
+    Homie.setNodeProperty(cylinderNode, "rawBottom", String(botAdc), true);
+
+    Homie.setNodeProperty(cylinderNode, "temperatureTop", String(topAdc), true);
+    Homie.setNodeProperty(cylinderNode, "temperatureMiddle", String(midAdc), true);
+    Homie.setNodeProperty(cylinderNode, "temperatureBottom", String(botAdc), true);
+
+    Homie.setNodeProperty(cupboardNode, "temperature", String(htu.readTemperature()), true);
+    Homie.setNodeProperty(cupboardNode, "humidity", String(htu.readHumidity()), true);
+
+    lastDataSent += TRANSMIT_INTERVAL * 1000UL;
   }
-  */
 }
 
 void onHomieEvent(HomieEvent event) {
   Serial.print("EVENT: ");
   Serial.println(event);
 
-  switch(event) {
+  switch(event)
+  {
     case HOMIE_CONFIGURATION_MODE:
       break;
     case HOMIE_NORMAL_MODE:
@@ -65,51 +87,63 @@ void onHomieEvent(HomieEvent event) {
   }
 }
 
-void setup() {
+void setup()
+{
   /*
   ESP.getResetReason() returns String containing the last reset resaon in human readable format.
-
   ESP.getFreeHeap() returns the free heap size.
-
   ESP.getChipId() returns the ESP8266 chip ID as a 32-bit integer.
+  ESP.getCycleCount() returns the cpu instruction cycle count since start as an unsigned 32-bit. This is useful for accurate timing of very short actions like bit banging.
 
   Several APIs may be used to get flash chip info:
-
   ESP.getFlashChipId() returns the flash chip ID as a 32-bit integer.
-
   ESP.getFlashChipSize() returns the flash chip size, in bytes, as seen by the SDK (may be less than actual size).
-
   ESP.getFlashChipRealSize() returns the real chip size, in bytes, based on the flash chip ID.
-
   ESP.getFlashChipSpeed(void) returns the flash chip frequency, in Hz.
-
-  ESP.getCycleCount() returns the cpu instruction cycle count since start as an unsigned 32-bit. This is useful for accurate timing of very short actions like bit banging.
   */
-  Serial.print("Flash chip size: ");
-  Serial.println(ESP.getFlashChipSize());
 
-  Wire.begin(0, 2);  // SDA=GPIO0 SCL=GPIO2
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
 
-  ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV
+  Wire.begin(D2, D1);  // SDA=D2=GPIO4 SCL=D1=GPIO5
+
+  ads.setGain(GAIN_ONE);        // 1x gain   1 bit = 2mV
   ads.begin();
+
+  if (!htu.begin())
+  {
+    Serial.println("Couldn't find HTU21D sensor!");
+  }
 
   Homie.setFirmware(FW_NAME, FW_VERSION);
 
   Homie.onEvent(onHomieEvent);
 
-  // nothing to set
-  //tempTopNode.subscribe("on", temp1Handler);
 
-  Homie.registerNode(tempTopNode);
-  Homie.registerNode(tempMidNode);
-  Homie.registerNode(tempBotNode);
+  Homie.registerNode(cylinderNode);
+  Homie.registerNode(collectorNode);
+  Homie.registerNode(cupboardNode);
 
   //Homie.setSetupFunction(homieSetupHandler);
   Homie.setLoopFunction(homieLoopHandler);
 
+  /*
+  Serial.println("Flash chip stats:");
+  Serial.print("  id: ");
+  Serial.println(ESP.getFlashChipId()); // returns the flash chip ID as a 32-bit integer.
+  Serial.print("  size: ");
+  Serial.println(ESP.getFlashChipSize());
+  Serial.print("  realsize: ");
+  Serial.println(ESP.getFlashChipRealSize());
+  Serial.print("  speed: ");
+  Serial.println(ESP.getFlashChipSpeed());
+  */
+
   Homie.setup();
 }
 
-void loop() {
+void loop()
+{
   Homie.loop();
 }
